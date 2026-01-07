f97c8d158e12993a6a5254aac1b40296a375c854
rohit <rohitgatne2300@gmail.com>
2026-01-07 12:57:51 +0530

    gmm

--- gmm.py
+++ gmm.py
@@ -10,27 +10,43 @@ class KMeans():
         n_samples, n_features = X.shape
         centroids = X[np.random.choice(n_samples, size=self.n_clusters)]
 
-    def _euclidean_dist(centroids, X):
+    def _assign_clusters(centroids, X):
         n_samples, n_features = X.shape
         distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=2)
         assignments = np.zeros_like(distances)
         assignments[np.arange(n_samples),distances.argmin(axis = 1)] = 1
+    
+
 
 class GaussianMixture():
-    def __init__(self, k_components=1, max_iter=100):
-        self.k = k_components
-        self.w = self.init_weights(k)        # normalize initial weights
-        self.means, self.var = None, None
-        self.resp = None
+    def __init__(self, n_components=1, max_iter=100):
+        self.k = n_components
+        self.means, self.covariances,self.resp ,self.weights = None, None,None,None
+        self.init_weights()
 
-    def init_weights(k):
-        w = np.ones((k,))/k
-        if not np.allclose(w.sum(), 1):
+    def init_weights(self,n_components):
+        self.w = np.ones((n_components,))/n_components
+        if not np.allclose(self.w.sum(), 1):
            print("Weights not normalized")
 
-        return w
     
-    def init_distributions(data, n_clusters):
+    def init_distributions(self,X, n_clusters):
         # initializing using k-means initialization
-        n_samples, n_features = data.shape
-        centroids = data[np.random.choice(n_samples,size=n_clusters)]
+        n_samples, n_features = X.shape
+        centroids = X[np.random.choice(n_samples,size=n_clusters)]
+
+        self.means = centroids
+        
+        distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=2)
+        assignments = np.zeros_like(distances)
+        assignments[np.arange(n_samples),distances.argmin(axis = 1)] = 1
+
+
+        self.covariances = np.empty(n_clusters, n_features, n_features)
+
+        for i in range(n_clusters):
+            self.covariances[i] = np.cov(X[assignments[:,i] == 1].T)
+    
+    def E_step(self, X):
+        self.resp = np.empty
+
